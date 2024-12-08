b58aeec26055ceab69ad87d169d38d09a5fbc847
Given-12-kn <given.l23@mhs.istts.ac.id>
2024-12-08 12:43:11 +0700

    picture

--- POS/POS/FormPayment.Designer.cs
+++ POS/POS/FormPayment.Designer.cs
@@ -30,6 +30,7 @@ namespace POS
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button10 = new System.Windows.Forms.Button();
             this.buttonEnter = new System.Windows.Forms.Button();
             this.button0 = new System.Windows.Forms.Button();
@@ -44,10 +45,11 @@ namespace POS
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
             this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -69,10 +71,18 @@ namespace POS
             this.panel1.Controls.Add(this.button3);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(697, 355);
+            this.panel1.Location = new System.Drawing.Point(1886, 147);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(542, 570);
             this.panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(539, 59);
+            this.textBox1.TabIndex = 13;
             // 
             // button10
             // 
@@ -206,11 +216,23 @@ namespace POS
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.panel4);
             this.panel2.Controls.Add(this.label1);
             this.panel2.Location = new System.Drawing.Point(0, -1);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(1903, 79);
             this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(263, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Amount (Rp ###)";
             // 
             // panel3
             // 
@@ -231,30 +253,29 @@ namespace POS
             this.crystalReportViewer1.Size = new System.Drawing.Size(469, 976);
             this.crystalReportViewer1.TabIndex = 0;
             // 
-            // textBox1
+            // panel4
             // 
-            this.textBox1.Location = new System.Drawing.Point(3, 41);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(539, 59);
-            this.textBox1.TabIndex = 13;
+            this.panel4.Location = new System.Drawing.Point(475, 78);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1119, 615);
+            this.panel4.TabIndex = 3;
             // 
-            // label1
+            // label2
             // 
-            this.label1.AutoSize = true;
-            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label1.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(263, 21);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(209, 37);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Amount (Rp ###)";
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(618, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Card";
             // 
             // FormPayment
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1902, 1055);
+            this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
@@ -269,6 +290,7 @@ namespace POS
             this.panel2.PerformLayout();
             this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -293,5 +315,7 @@ namespace POS
         private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label2;
     }
 }
