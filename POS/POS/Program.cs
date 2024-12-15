519398c46b29952f70685e678e30ec6262d46fc2
egyw <152834874+egyw@users.noreply.github.com>
2024-12-15 23:18:05 +0700

    +++

--- POS/POS/Program.cs
+++ POS/POS/Program.cs
@@ -16,7 +16,7 @@ namespace POS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormAdmin());
+            Application.Run(new FormPayment());
         }
     }
 }
