9984fda0e8297e92d66397d5bb807fa6c1e53614
Winstonjie <jiewandana888@gmail.com>
2024-12-23 18:59:51 +0700

    fix db

--- POS/POS/Program.cs
+++ POS/POS/Program.cs
@@ -16,7 +16,7 @@ namespace POS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormAdminEgy("eggy"));
+            Application.Run(new FormMain(3));
         }
     }
 }
